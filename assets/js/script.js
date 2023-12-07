4d843714672d40e29cf0d0b5a9e18071e0127439
e1ixyz <elimcgehee2001@gmail.com>
2023-12-06 19:00:00 -0500

    fix

--- assets/js/script.js
+++
@@ -1,6 +0,0 @@
-$(window).load(function () {
-  $(".goog-logo-link").parent().remove();
-  $(".goog-te-gadget").html(
-    $(".goog-te-gadget").html().replace('&nbsp;&nbsp;Powered by ', '')
-  );
-});
